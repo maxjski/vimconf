5d95f7826a76b7c523860f6033d4b8aa2c79477c
Maksymilian Jankowski <ma.ad.jank@gmail.com>
2025-10-15 19:37:59 +0100

    ctrl backspace

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,7 @@
 -- Center view after Ctrl+d and Ctrl+u
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, desc = "Half page down and center" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, desc = "Half page up and center" })
+
+-- Ctrl+Backspace to delete previous word in insert mode
+vim.keymap.set("i", "<C-H>", "<C-w>", { noremap = true, desc = "Delete previous word" })
+vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true, desc = "Delete previous word" })
