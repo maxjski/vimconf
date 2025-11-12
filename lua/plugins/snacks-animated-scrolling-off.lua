b63b6dd5e75c04cd2e1acaf2c43b0c0edf0ce911
Maksymilian Jankowski <ma.ad.jank@gmail.com>
2025-11-12 18:16:50 +0000

    some solidity stuff

--- lua/plugins/snacks-animated-scrolling-off.lua
+++ lua/plugins/snacks-animated-scrolling-off.lua
@@ -2,7 +2,12 @@ return {
 	"folke/snacks.nvim",
 	opts = {
 		scroll = {
-			enabled = false, -- Disable scrolling animations
+			enabled = false,
 		},
+		-- Disable Snacks replacements for vim.ui.select/input to avoid empty pickers
+		select = { enabled = false },
+		input = { enabled = false },
+		-- Fully disable Snacks picker to avoid unexpected popups on open
+		picker = { enabled = false },
 	},
 }
---
+++ lua/plugins/solidity.lua
@@ -0,0 +1,4 @@
+-- Solidity support disabled
+-- The LSP requires solc compiler which causes spawn errors
+-- Treesitter syntax highlighting also disabled to avoid any issues
+return {}
